4ee8a94d42f796d5b74c6d226a5d1fc32b809462
montekaka <josh.chen.jia@gmail.com>
2018-11-25 21:35:57 -0800

    add get product/:id route

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,7 @@ const controller = require('./controllers');
 const router = require('express').Router();
 
 router.get('/api/products', controller.products.get);
+router.get('/api/products/:id', controller.products.get);
 router.post('/api/products', controller.products.post);
 router.put('/api/products/:id', controller.products.put);
 
