feae4b5e1356352e6ed428df05ef1c3c7e935f43
montekaka <josh.chen.jia@gmail.com>
2018-11-26 22:00:34 -0800

    add suggestion get and delete endpoint

--- server/routes.js
+++ server/routes.js
@@ -7,4 +7,8 @@ router.post('/api/products', controller.products.post);
 router.put('/api/products/:id', controller.products.put);
 router.delete('/api/products/:id', controller.products.delete);
 
+
+router.get('/api/suggestions', controller.suggestions.get);
+router.delete('/api/suggestions/:id', controller.suggestions.delete);
+
 module.exports = router;
