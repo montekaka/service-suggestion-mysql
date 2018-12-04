9bf40f868095fd1b920d3e3cdabcb2bb23466b6b
montekaka <josh.chen.jia@gmail.com>
2018-12-03 16:45:57 -0800

    change to bulkcreate

--- server/db/seed.js
+++ server/db/seed.js
@@ -8,20 +8,28 @@ const fake = require('./../../libs/fake.js');
 
 const dataGenerator = fake.generator;
 
-var generateData = (n) => {
+var generateData = (n, k) => {
 	var p = Promise.resolve();
-	var products = [];
-	for(var i = 0; i < n; i++) {
-		let data = dataGenerator();
+	var e = n / k;
+	for(var j = 0; j < e; j++) {
+		var products = [];
+		for(var i = 0; i < k; i++) {			
+			let data = dataGenerator();
+			products.push(data);			
+		}
 		p = p.then(() => {
-			return Product.create(data).then(() => {
+			return Product.bulkCreate(products).then(() => {
+				console.log(`bulkCreate ${j}`);
 			});
-		})
-	}		
+		});		
+	}
 	return p;
 }
 
-generateData(100).then(() => {
+// print start time
+// generateData(10000000, 100000) # 20min
+generateData(10000000, 10000).then(() => {
 	console.log('done');
+	// console.log end time,,, please ...
 	process.exit();		
 });
