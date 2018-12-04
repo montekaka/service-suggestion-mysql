3ff83c3c5b6863eedbf3bcd3d84f8e81a71ba990
montekaka <josh.chen.jia@gmail.com>
2018-12-03 21:54:43 -0800

    add insert suggestions

--- server/db/seed.js
+++ server/db/seed.js
@@ -10,25 +10,7 @@ const fake = require('./../../libs/fake.js');
 
 const dataGenerator = fake.generator;
 
-// const insertProducts = (n, k) => {
-// 	var p = Promise.resolve();
-// 	var e = n / k;
-// 	for(var j = 0; j < e; j++) {
-// 		p = p.then(() => {
-// 			var products = [];
-// 			for(var i = 0; i < k; i++) {			
-// 				let data = dataGenerator();
-// 				products.push(data);			
-// 			}
-// 			return Product.bulkCreate(products).then(() => {
-// 				console.log(`bulkCreated`);
-// 			});
-// 		});		
-// 	}
-// 	return p;
-// }
-
-const insertPromiseProducts = (n, k) => {
+const insertProducts = (n, k) => {
 	var e = n / k;
 	const data = [];
 	const startTime = Date.now();
@@ -44,7 +26,7 @@ const insertPromiseProducts = (n, k) => {
 		data.push(generateProducts);
 	}
 
-	Promise.map(data, (products) => {
+	return Promise.map(data, (products) => {
 		return Product.bulkCreate(products()).then(() => {
 			console.log('inserted')
 		})
@@ -52,15 +34,63 @@ const insertPromiseProducts = (n, k) => {
 		concurrency: 1
 	})
 	.then(() => {
-		console.log('done...');
-		const endTime = Date.now();
-		console.log(`finished in ${endTime}, ${startTime}`);
-		process.exit();
+		// console.log('done...');
+		// const endTime = Date.now();
+		// console.log(`finished in ${endTime}, ${startTime}`);
+		// process.exit();
+		return startTime;
 	})
 }
 
-//const startTime = Date.now();
-insertPromiseProducts(10000000, 1000);
+insertSuggestions = (n) => {
+	//Product.findAll({offset: 0 , limit: 1})
+	const offsets = [];
+	for(var i = 0; i < n; i++) {
+		offsets.push(i);
+	};
+	return Promise.map(offsets, (idx) => {
+		return Product.findAll({offset: idx , limit: 1}).then((product) => {
+			let name = product[0]['name'];
+			let id = product[0]['id'];
+
+			return Promise.map(offsets, (idy) => {
+				return Product.findAll({offset: idy , limit: 1}).then((suggestProduct) => {
+					let _name = suggestProduct[0]['name'];
+					let _id = suggestProduct[0]['id'];
+					var bulk = [];
+					if(_id !== id) {
+						// add suggestions
+						let score = stringSimilarity.compareTwoStrings(_name, name);
+						bulk.push({ ProductId: id, suggestProductId: _id, score: score });
+						bulk.push({ ProductId: _id, suggestProductId: id, score: score });
+						Suggestion.bulkCreate(bulk).then(() => {
+							console.log('inserted suggestions')
+						});
+					}
+				})
+			}, {
+				concurrency: 1
+			}).then(() => {
+				return true;
+			})
+
+		})
+	}, {
+		concurrency: 1
+	}).then(() => {
+		return true;
+	})
+}
+
+insertSuggestions(100).then(() => {
+	console.log('done...');
+	process.exit();	
+});
+
+// insertProducts(100, 10).then((startTime) => {
+// 	console.log('done...', startTime);
+// 	process.exit();
+// });
 
 // print start time
 // generateData(10000000, 10000) # 1min
